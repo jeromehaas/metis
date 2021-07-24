938712a1ec5dbb48d97883a6e38be58c09621306
Jerome Haas <hello@jeromehaas.dev>
2021-07-24 21:51:06 +0200

    Implement: getUsername function

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
-const functions = require('./lib/functions')
-const settings = require('./settings.json')
+
+const getUsername = require('./lib/get-username');
+const functions = require('./lib/functions');
+const settings = require('./settings.json');
 const clear = require('clear');
 
 const run = async () => {
   clear();
-  await functions.createNewNote();
+	const username = getUsername();	
+	console.log(username);
 }
 
 run();
